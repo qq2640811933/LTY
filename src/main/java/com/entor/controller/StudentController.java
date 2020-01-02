bdd853f242331a054639f12690d5111767b293f8
Administrator <Administrator@80DR054LIU0TTAN>
2020-01-02 16:42:52 +0800

    添加方法

--- src/main/java/com/entor/controller/StudentController.java
+++ src/main/java/com/entor/controller/StudentController.java
@@ -26,8 +26,7 @@ public class StudentController {
 		
 	}
 	@RequestMapping("/deleteById")
-	public void deleteById(int id) {
-	
+	public void deleteById(int id) {	
 		studentService.deleteById(id);
 	}
 	@RequestMapping("/queryById")
